90df74357427dadbb6cbd56397988c8dcf5401c5
Sam Daulton <samdaulton@instagram.com>
2022-02-14 23:39:48 +0000

    Create constraint_active_search.py

---
+++ robust_mobo/constraint_active_search.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+r"""
+CAS based on the BoTorch tutorial.
+"""
+
+import torch
+from botorch.acquisition.monte_carlo import MCAcquisitionFunction
+from botorch.acquisition.objective import IdentityMCObjective
+from botorch.models import ModelListGP
+from botorch.utils.sampling import sample_hypersphere
+from botorch.utils.transforms import t_batch_mode_transform
+
+
+def smooth_mask(x, a, eps=2e-3):
+    """Returns 0ish for x < a and 1ish for x > a"""
+    return torch.nn.Sigmoid()((x - a) / eps)
+
+
+def smooth_box_mask(x, a, b, eps=2e-3):
+    """Returns 1ish for a < x < b and 0ish otherwise"""
+    return smooth_mask(x, a, eps) - smooth_mask(x, b, eps)
+
+
+def identify_samples_which_satisfy_constraints(X, constraints):
+    """
+    Takes in values (a1, ..., ak, o) and returns (a1, ..., ak, o)
+    True/False values, where o is the number of outputs.
+    """
+    successful = torch.ones(X.shape).to(X)
+    for model_index in range(X.shape[-1]):
+        these_X = X[..., model_index]
+        direction, value = constraints[model_index]
+        successful[..., model_index] = (
+            these_X < value if direction == "lt" else these_X > value
+        )
+    return successful
+
+
+class ExpectedCoverageImprovement(MCAcquisitionFunction):
+    def __init__(
+        self,
+        model,
+        constraints,
+        punchout_radius,
+        bounds,
+        tkwargs: dict,
+        num_samples=512,
+        **kwargs,
+    ):
+        """Expected Coverage Improvement (q=1 required, analytic)
+        Right now, we assume that all the models in the ModelListGP have
+        the same training inputs.
+        Args:
+            model: A ModelListGP object containing models matching the corresponding constraints.
+                All models are assumed to have the same training data.
+            constraints: List containing 2-tuples with (direction, value), e.g.,
+                [('gt', 3), ('lt', 4)]. It is necessary that
+                len(constraints) == model.num_outputs.
+            punchout_radius: Positive value defining the desired minimum distance between points
+            bounds: torch.tensor whose first row is the lower bounds and second row is the upper bounds
+            tkwargs: Tensor arguments.
+            num_samples: Number of samples for MC integration
+        """
+        assert isinstance(model, ModelListGP)
+        super().__init__(model=model, objective=IdentityMCObjective(), **kwargs)
+        assert len(constraints) == model.num_outputs
+        assert all(direction in ("gt", "lt") for direction, _ in constraints)
+        assert punchout_radius > 0
+        self.constraints = constraints
+        self.punchout_radius = punchout_radius
+        self.bounds = bounds
+        self.base_points = self._identify_base_points_from_train_inputs()
+        self.ball_of_points = self._generate_ball_of_points(
+            num_samples=num_samples,
+            radius=punchout_radius,
+            tkwargs=tkwargs,
+        )
+        self._thresholds = torch.tensor(
+            [threshold for _, threshold in self.constraints]
+        ).to(bounds)
+        assert (
+            all(ub > lb for lb, ub in self.bounds.T) and len(self.bounds.T) == self.dim
+        )
+
+    @property
+    def num_outputs(self):
+        return self.model.num_outputs
+
+    @property
+    def dim(self):
+        return self.train_inputs.shape[-1]
+
+    @property
+    def train_inputs(self):
+        return self.model.models[0].train_inputs[0]
+
+    def _identify_base_points_from_train_inputs(self):
+        train_targets = [t.unsqueeze(-1) for t in self.model.train_targets]
+        untransformed_train_targets = (
+            [  # Untransform a potential outcome transform to get raw values
+                m.outcome_transform.untransform(t)[0]
+                for t, m in zip(train_targets, self.model.models)
+                if m.outcome_transform
+            ]
+        )
+        untransformed_train_targets = torch.cat(untransformed_train_targets, dim=-1)
+        satisfy_constraints = self._identify_samples_which_satisfy_constraints(
+            untransformed_train_targets
+        )
+        return self.train_inputs[satisfy_constraints.all(axis=-1)]
+
+    def _generate_ball_of_points(
+        self,
+        num_samples,
+        radius,
+        tkwargs,
+    ):
+        """Creates a ball of points to be used for MC."""
+        z = sample_hypersphere(d=self.dim, n=num_samples, qmc=True, **tkwargs)
+        r = torch.rand(num_samples, 1, **tkwargs) ** (1 / self.dim)
+        return radius * r * z
+
+    def _identify_samples_which_satisfy_constraints(self, X):
+        return identify_samples_which_satisfy_constraints(X, self.constraints)
+
+    def _get_base_point_mask(self, X):
+        distance_matrix = torch.cdist(X, self.base_points)
+        return smooth_mask(distance_matrix, self.punchout_radius)
+
+    def _estimate_probabilities_of_satisfaction_at_points(self, points):
+        """Estimate the probability of satisfying the given constraints."""
+        posterior = self.model.posterior(X=points)
+        mus, sigma2s = posterior.mean, posterior.variance
+        dist = torch.distributions.normal.Normal(mus, sigma2s.sqrt())
+        norm_cdf = dist.cdf(self._thresholds)
+        probs = torch.ones(points.shape[:-1]).to(points)
+        for i, (direction, _) in enumerate(self.constraints):
+            probs = probs * (
+                norm_cdf[..., i] if direction == "lt" else 1 - norm_cdf[..., i]
+            )
+        return probs
+
+    @t_batch_mode_transform(expected_q=1)
+    def forward(self, X):
+        """Evaluate Expected Improvement on the candidate set X."""
+        ball_around_X = self.ball_of_points + X
+        domain_mask = smooth_box_mask(
+            ball_around_X, self.bounds[0, :], self.bounds[1, :]
+        ).prod(dim=-1)
+        num_points_in_integral = domain_mask.sum(dim=-1)
+        base_point_mask = self._get_base_point_mask(ball_around_X).prod(dim=-1)
+        prob = self._estimate_probabilities_of_satisfaction_at_points(ball_around_X)
+        masked_prob = prob * domain_mask * base_point_mask
+        y = masked_prob.sum(dim=-1) / num_points_in_integral
+        return y
